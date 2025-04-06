3f8062a4f5346211b402c6c2b8b5da286ba68341
nabusim <cristianjose@gmail.com>
2025-04-06 20:59:34 +0200

    Initial Commit

---
+++ MangaSama25/MangaSama25App.swift
@@ -0,0 +1,17 @@
+//
+//  MangaSama25App.swift
+//  MangaSama25
+//
+//  Created by Cristian Jose Perez Guerra on 06/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MangaSama25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
